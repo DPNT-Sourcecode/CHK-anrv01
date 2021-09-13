2e821d0778578d734c96ab4fb022e73335a9fc6e
sbx_user1051 <sbx_user1051@169.254.97.53>
2021-09-13 19:35:08 +0000

    Mon Sep 13 19:35:08 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,10 +18,11 @@ def checkout(skus):
         if sku_values.get(char):
             occurrences[char] = occurrences[char] + 1
             total_cost += sku_values.get(char)
-    if occurrences["A"] == 3:
-        total_cost -= 20
-    if occurrences["B"] == 2:
-        total_cost -= 15
+    if occurrences["A"] % 3:
+        total_cost -= 20 * occurrences["A"] % 3
+    if occurrences["B"] % 2:
+        total_cost -= 15 * occurrences["B"] % 2
 
     return total_cost
+
 
