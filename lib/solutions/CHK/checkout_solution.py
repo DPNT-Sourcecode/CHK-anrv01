799b3627e0ad852f5083283b66fe0e1fa151566e
sbx_user1051 <sbx_user1051@169.254.97.53>
2021-09-13 20:09:14 +0000

    Mon Sep 13 20:09:14 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,11 +7,11 @@ from math import floor
 
 def apply_discount(sku, occurrence_data, number_required, discount_applied_per_offer, total_cost):
     if occurrence_data[sku] % number_required > 0:
-        total_discount = discount_applied_per_offer * occurrence_data[sku]
+        total_discount = discount_applied_per_offer * floor(occurrence_data[sku] / number_required)
         occurrence_data[sku] = occurrence_data[sku] - number_required * occurrence_data[sku] % number_required
         return total_cost - total_discount, occurrence_data
     else:
-        return 0
+        return total_cost, occurrence_data
 
 
 def checkout(skus):
@@ -38,4 +38,5 @@ def checkout(skus):
     total_cost, occurrences = apply_discount("B", occurrences, 2, 15, total_cost)
 
     return total_cost
+
 
