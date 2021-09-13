9387ac12ce12909ad767e90de19faa64d49a3157
sbx_user1051 <sbx_user1051@169.254.97.53>
2021-09-13 19:32:08 +0000

    Mon Sep 13 19:32:08 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,25 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    if not isinstance(skus, str):
+        return -1
+    sku_values = {"A": 50,
+                  "B": 30,
+                  "C": 20,
+                  "D": 15}
+    occurrences = {"A": 0,
+                   "B": 0,
+                   "C": 0,
+                   "D": 0}
+    total_cost = 0
+    for char in skus:
+        if sku_values.get(char):
+            occurrences[char] = occurrences[char] + 1
+            total_cost += sku_values.get(char)
+    if occurrences["A"] == 3:
+        total_cost -= 20
+    if occurrences["B"] == 2:
+        total_cost -= 15
+
+    return total_cost
+
--- test/solution_tests/CHK/test_round1.py
+++ test/solution_tests/CHK/test_round1.py
@@ -5,4 +5,14 @@ class Test:
     def test_read_functionality(self):
         input_skus = "A B CDAD,C"
         total_value = checkout(input_skus)
-        assert total_value == 
+        assert total_value == 200
+
+    def test_bad_input(self):
+        input_skus = 400
+        total_value = checkout(input_skus)
+        assert total_value == -1
+
+    def test_special_offer(self):
+        input_skus = "AABA, B"
+        total_value = checkout(input_skus)
+        assert total_value == 175
